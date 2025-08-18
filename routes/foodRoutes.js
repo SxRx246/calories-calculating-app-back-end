5915334507142d9936447e3377397a9037f5d41f
Sara Alqallaf <202107125@stu.uob.edu.bh>
2025-08-18 16:59:06 +0300

    dont know what we did

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -6,8 +6,6 @@ const FoodController = require('../controllers/Food')
 router.post('/new' , FoodController.createFood)
 router.get('/' , FoodController.allFood)
 router.get('/:id' , FoodController.showFoodDetails)
-router.put('/:id',FoodController.updatedFood)
-router.delete('/:id',FoodController.deleteFood)
 
 
 module.exports = router
