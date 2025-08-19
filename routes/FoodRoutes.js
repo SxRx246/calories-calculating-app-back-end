0d7579c64a2b5c398e3fe1d343a70a52dd404020
SxRx246 <132745892+SxRx246@users.noreply.github.com>
2025-08-20 00:01:10 +0300

    Update FoodRoutes.js

--- routes/FoodRoutes.js
+++ routes/FoodRoutes.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const multer = require("multer")
+const path = require("path")
 
 const FoodController = require('../controllers/Food')
 
-router.post('/new' , FoodController.createFood)
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/") 
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname
+    cb(null, uniqueName)
+  }
+})
+
+const upload = multer({ storage })
+
+
+
+router.post('/new' , upload.single("picture"), FoodController.createFood)
 router.get('/' , FoodController.allFood)
 router.get('/:id' , FoodController.showFoodDetails)
+router.put('/:id', upload.single("picture"), FoodController.updatedFood)
+router.delete('/:id',FoodController.deleteFood)
 
 
 module.exports = router
